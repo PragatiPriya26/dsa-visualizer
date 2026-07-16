2643ae2213d2b66eaba752e4b5cf051481d39b9a
P-PRIYA-ai <pragatipriya2626@gmail.com>
2026-07-16 19:35:14 +0530

    navbar

---
+++ dsa-visualizer/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <nav className="bg-slate-800 text-white shadow-lg">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+        <h1 className="text-3xl font-bold text-cyan-400">
+          DSA Visualizer
+        </h1>
+
+        <p className="text-gray-300">
+          Learn Sorting Algorithms Visually
+        </p>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
